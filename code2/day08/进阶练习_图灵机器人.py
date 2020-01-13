5fa5310a6630c0de296e666f93a9fad7387d3dbc
tsh <1134512154@qq.com>
2020-01-14 00:44:20 +0800

    第29天学习,图灵机器人

--- code2/day08/进阶练习_图灵机器人.py
+++ code2/day08/进阶练习_图灵机器人.py
@@ -10,4 +10,37 @@
 
 注册登录，才能创建自己的图灵机器人。
 根据帮助中心的“说明书”，我们可以了解如何运用这个新工具～
+文档地址: https://www.kancloud.cn/turing/www-tuling123-com/718227
 """
+import requests
+import json
+
+url = 'http://openapi.tuling123.com/openapi/api/v2'
+data = {
+    "reqType": 0,
+    "perception": {
+        "inputText": {
+            "text": "附近的酒店"
+        },
+        "inputImage": {
+            "url": "imageUrl"
+        },
+        "selfInfo": {
+            "location": {
+                "city": "北京",
+                "province": "北京",
+                "street": "信息路"
+            }
+        }
+    },
+    "userInfo": {
+        "apiKey": "b13f44a2adc54a64b760b396afaf4e77",
+        # "apiKey": "0ac325b4dad74f38830b24cb5fc1ced6",
+        "userId": "136311"
+    }
+}
+res = requests.post(url, data=data)
+if res.status_code == 200:
+    print(res.text)
+else:
+    print('请求失败!', url)
