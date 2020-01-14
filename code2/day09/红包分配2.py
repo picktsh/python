22160f38898192009122b02d041591b67c5cfdc3
tsh <1134512154@qq.com>
2020-01-15 02:14:42 +0800

    第29天学习,研究红包分配-进阶

--- code2/day09/红包分配2.py
+++ code2/day09/红包分配2.py
@@ -1,13 +1,14 @@
 import random
 
 
-def red(m, n):
-    m = m * 100
-    li = random.sample(range(1, m), n - 1)
-    li = li + [0, m]
+def red(money, num):
+    money = int(money * 100)
+    li = random.sample(range(1, money), num - 1)
+    li = li + [0, money]
     li.sort()
-    nli = [(li[i + 1] - li[i]) / 100 for i in range(len(li) - 1)]
-    return nli
+    return [(li[i + 1] - li[i]) / 100 for i in range(len(li) - 1)]
 
 
-print(red(5, 2))
+m = float(input('老板想发多少元的红包?'))
+n = int(input('发多少个呢?'))
+print('给您分配的方案:', red(m, n))
