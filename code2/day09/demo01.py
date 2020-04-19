b7c060dafd68750ef8bf86fad4f922f56d28fe57
tsh <1134512154@qq.com>
2020-04-19 21:37:47 +0800

    09 指挥浏览器自动工作

---
+++ code2/day09/demo01.py
@@ -0,0 +1,18 @@
+# 本地Chrome浏览器设置方法
+from selenium import  webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get('https://localprod.pandateacher.com/python-manuscript/hello-spiderman/')
+time.sleep(2)
+
+teacher = driver.find_element_by_id('teacher')
+teacher.send_keys('必须是吴枫呀')
+assistant = driver.find_element_by_name('assistant')
+assistant.send_keys('都喜欢')
+time.sleep(1)
+button = driver.find_element_by_class_name('sub')
+time.sleep(1)
+button.click()
+time.sleep(1)
+driver.close()
