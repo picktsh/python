8d07cb9aed95b4c5fc879817c9f5dcc144a94e9b
tsh <1134512154@qq.com>
2020-05-09 02:02:50 +0800

    day09 第一个练习的注解

--- code2/day09/demo01.py
+++ code2/day09/demo01.py
@@ -1,19 +1,19 @@
 # 本地Chrome浏览器设置方法
-from selenium import webdriver
-import time
+from selenium import webdriver  # 从selenium库中调用webdriver模块
+import time  # 调用time模块
 
-driver = webdriver.Chrome()
-driver.get('https://localprod.pandateacher.com/python-manuscript/hello-spiderman/')
-time.sleep(2)
+driver = webdriver.Chrome()  # 设置引擎为Chrome，真实地打开一个Chrome浏览器
+driver.get('https://localprod.pandateacher.com/python-manuscript/hello-spiderman/')  # 访问页面
+time.sleep(2)  # 暂停两秒，等待浏览器缓冲
 
-teacher = driver.find_element_by_id('teacher')
-teacher.send_keys('必须是吴枫呀')
-assistant = driver.find_element_by_name('assistant')
-assistant.send_keys('都喜欢')
+teacher = driver.find_element_by_id('teacher')  # 找到【请输入你喜欢的老师】下面的输入框位置
+teacher.send_keys('必须是吴枫呀')  # 输入文字
+assistant = driver.find_element_by_name('assistant')  # 找到【请输入你喜欢的助教】下面的输入框位置
+assistant.send_keys('都喜欢')  # 输入文字
 time.sleep(1)
-button = driver.find_element_by_class_name('sub')
+button = driver.find_element_by_class_name('sub')  # 找到【提交】按钮
 time.sleep(1)
-button.click()
+button.click()  # 点击【提交】按钮
 time.sleep(1)
-driver.close()
+driver.close()  # 关闭浏览器
 
