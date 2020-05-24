0a6f20b8499bf5aab5ef7f76e98c6502952cbedf
rise <rise.666@qq.com>
2020-05-24 20:37:14 +0800

    day14 爬取并储存

--- code2/day14/jobui/jobui/items.py
+++ code2/day14/jobui/jobui/items.py
@@ -9,6 +9,7 @@ import scrapy
 
 
 class JobuiItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    company = scrapy.Field()
+    position = scrapy.Field()
+    address = scrapy.Field()
+    detail = scrapy.Field()
--- code2/day14/jobui/jobui/settings.py
+++ code2/day14/jobui/jobui/settings.py
@@ -16,10 +16,10 @@ NEWSPIDER_MODULE = 'jobui.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'jobui (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
